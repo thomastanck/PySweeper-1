1b175a2765f6cde1416358aab6d7d8d1f8861c35
Thomas Tan <thomastanck@gmail.com>
2016-10-27 15:14:39 +0800

    Clean up a bit

--- pysweep/event.py
+++ pysweep/event.py
@@ -3,13 +3,12 @@ class Event:
     Supposed to be some parent class or something. "saa..."
     """
     def __init__(self, *args, **kwargs):
-        print("Event init'd")
         self.args = args
         self.kwargs = kwargs
         self.children = []
 
     def __str__(self):
-        return ("{" + "args:{},kwargs:{},children:{}".format(
+        return ("{" + "args:{},kwargs:{},children:\n{}\n".format(
             self.args,
             self.kwargs,
             self.children
