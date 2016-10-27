63a42a97220b595dbc210c5cd970434a04dff9cd
Thomas Tan <thomastanck@gmail.com>
2016-10-27 14:51:44 +0800

    Debug event

--- pysweep/event.py
+++ pysweep/event.py
@@ -3,6 +3,7 @@ class Event:
     Supposed to be some parent class or something. "saa..."
     """
     def __init__(self, *args, **kwargs):
+        print("Event init'd")
         self.args = args
         self.kwargs = kwargs
         self.children = []
